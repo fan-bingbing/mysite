12ff4661f88785c5199944230096f79bb820ac84
fansunsun <fanyu1980@hotmail.com>
2019-08-15 15:52:39 +1000

    fifth snapshot

--- polls/views.py
+++ polls/views.py
@@ -1,22 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse
-from django.template import loader
+
+
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
+
     context = {
-        'latest_question_list': latest_question_list,
+        'latest_question_list': latest_question_list
     }
-    # output = ', '.join([q.question_text for q in latest_question_list])
-    # return HttpResponse(output)
-    return HttpResponse(template.render(context, request))
+
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse(f"You are looking at question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     return HttpResponse(f"You are looking at results of question {question_id}")
